7dc72a7f96f8fc5c26f5fd1f7d1f42b0768d3328
thatcoderisfire <hooper.rachel@outlook.com>
2025-01-16 14:31:29 -0800

    Added JavaScript, corrected errors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Seann sgìrean bha pròiseil làn shràidean len còsan",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);

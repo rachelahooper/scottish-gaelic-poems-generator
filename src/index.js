df3ef2514cb763dec9dc83de40b70c0ad0e0abab
thatcoderisfire <hooper.rachel@outlook.com>
2025-01-17 14:03:10 -0800

    Added API/AI integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "Seann sgìrean bha pròiseil làn shràidean len còsan",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "3bfca328927aedo94ct2bc3eb0f193b8";
+  let prompt = `Generate a short but beautiful Scottish Gaelic poem about ${instructionsInput.value}`;
+  let context = `User Instructions: Generate a Scottish Gaelic poem about ${instructionsInput.value}.You are a talented poet and you love to write short poems that honor a scottish heritage. Your mission is to generate a short four line poem formatted in basic HTML - do NOT start the poem with 'html'. Please include a <br /> between each line, and sign with 'SheCodes AI' inside a <strong> element.Make sure to follow the user instructions`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem...");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
